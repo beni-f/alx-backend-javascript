e865c3148acabbf60986413ab35cb41df10bb736
beni-f <benifissha@gmail.com>
2024-05-29 16:50:03 +0300

    updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from "./utils";
 function handleProfileSignup(){
     return Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
-        const[photoResponse, userResponse] = responses;
-        console.log(`${photoResponse.body}, ${userResponse.firstName}, ${userResponse.lastName}`);
+        console.log(responses);
+        console.log(`${responses[0].body}, ${responses[1].firstName}, ${responses[1].lastName}`);
     }).catch((error)=> {
         console.error("Signup system offline");
     })
