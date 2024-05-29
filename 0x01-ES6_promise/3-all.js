e99ecaaa4e89bfd50acc7a85ebc5ad44891dcd80
beni-f <benifissha@gmail.com>
2024-05-29 15:21:16 +0300

    3-all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from "./utils";
+function handleProfileSignup(){
+    return Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+        const[photoResponse, userResponse] = responses;
+        console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    }).catch((error)=> {
+        console.error('Error:', error);
+    })
+}
+
+export default handleProfileSignup;
