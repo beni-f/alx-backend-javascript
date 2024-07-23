2ce1f20e166d25cc3d596ca94e72b3ec3fc11a0c
beni-f <benifissha@gmail.com>
2024-07-23 22:40:58 +0300

    3-read_file_async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+        const data = await fs.readFile(path, 'utf-8');
+
+        const lines = data.trim().split('\n').filter(line => line.trim());
+
+        const header = lines[0].split(',').map(field => field.trim());
+        const students = lines.slice(1);
+
+        const fieldMap = {};
+
+        students.forEach(student => {
+            const fields = student.split(',').map(field => field.trim());
+
+            header.forEach((field, idx) => {
+                const fieldName = field;
+                const fieldValue = fields[idx];
+
+                if (fieldName === 'field' && fieldValue) {
+                    if (!fieldMap[fieldValue]) {
+                        fieldMap[fieldValue] = [];
+                    }
+                    fieldMap[fieldValue].push(fields[0])
+                }
+            });
+        });
+        const totalStudents = students.length;
+        console.log(`Number of students: ${totalStudents}`);
+
+        Object.keys(fieldMap).forEach(field => {
+            console.log(`Number of students in ${field}: ${fieldMap[field].length}. List: ${fieldMap[field].join(', ')}`); 
+        });
+    } catch (error) {
+        throw new Error('Cannot load the database')
+    }
+}
