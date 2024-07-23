da26eb5f79124ec320d897d98ceb292ed2dee835
beni-f <benifissha@gmail.com>
2024-07-23 21:20:44 +0300

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displayes a message on the STDOUT
+ * @param {String} message The message to be displayed
+ *  
+*/
+const displayMessage = (message) => {
+    console.log(message);
+}
+
+module.exports = displayMessage;
