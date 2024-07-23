bfdd2fc7e2e89c98bfc5f8cf9b5b092bcafd6fbd
beni-f <benifissha@gmail.com>
2024-07-23 21:39:01 +0300

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
-process.stdin.setEncoding('utf-8')
-
-process.stdin.on('data', (data) => {
-    const name = data.trim();
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    process.exit();
+process.stdin.on('readable', () => {
+    const name = process.stdin.read()
+    if (name) {
+        process.stdout.write(`Your name is: ${name}`);
+    }
 });
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n')
+})
