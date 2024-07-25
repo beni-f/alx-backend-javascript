c6b1e5988987096098c8c3c6714c697a71ee43ec
beni-f <benifissha@gmail.com>
2024-07-25 22:22:08 +0300

    updated

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,16 +4,25 @@ const calculateNumber = require('./1-calcul')
 const expect = chai.expect
 
 describe("calculateNumber", () => {
-    it('Check if sum works', () => {
-        expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
+    describe("sum", () => {
+        it('Check if sum works', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
+        })
     })
-    it('Check if subtraction works', () => {
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
+
+    describe("subtract", () => {
+        it('Check if subtraction works', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
+        })
     })
-    it('Check if division works', () => {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
-    })
-    it('Check if catches error while invalid division', () => {
-        expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal("Error")
+
+    describe("divide", () => {
+        it('Check if division works', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
+        })
+
+        it('Check if catches error while invalid division', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal("Error")
+        })
     })
 })
