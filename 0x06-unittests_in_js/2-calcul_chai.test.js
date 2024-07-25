d1d16230db42a78ca6f58fe0c02b1299fe13e175
beni-f <benifissha@gmail.com>
2024-07-25 22:24:40 +0300

    updated

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,23 +6,23 @@ const expect = chai.expect
 describe("calculateNumber", () => {
     describe("sum", () => {
         it('Check if sum works', () => {
-            expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6)
         })
     })
 
     describe("subtract", () => {
         it('Check if subtraction works', () => {
-            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4)
         })
     })
 
     describe("divide", () => {
         it('Check if division works', () => {
-            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2)
         })
 
         it('Check if catches error while invalid division', () => {
-            expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal("Error")
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal("Error")
         })
     })
 })
