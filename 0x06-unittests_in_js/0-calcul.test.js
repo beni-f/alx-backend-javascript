a94d602f4f7788f3ff2586f075cfe56222108ef2
beni-f <benifissha@gmail.com>
2024-07-25 21:30:54 +0300

    updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -14,4 +14,12 @@ describe("calculateNumber", () => {
     it('should return 6 for 1.5 + 3.7', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6)
     })
+
+    it('rounding up a\'s floating point fractional numbers', () => {
+        assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+      });
+    
+    it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+        assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+    });
 })
